bda03854de198374af38dd6e84b34de1734c2ed2
Jugal Manjeshwar <jugalm9@gmail.com>
2014-01-31 18:11:35 -0500

    Update Implementation of Server owner Fix UI of self messages

--- js/main.js
+++ js/main.js
@@ -47,12 +47,13 @@ if(!config_data || !('username' in config_data) || config_data['username'].trim(
 
 	createProfile.addEventListener("click", function(){
 		if(!config_data) config_data = {};
-		config_data['username'] = createProfileText.value;
+		config_data['username'] = createProfileText.value.trim();
 		Config.write(config_data);
 		hideElement(welcomeProfile);
 		showElement(welcomeButtons);
 		titleProfile.innerHTML = config_data['username'];
 	});
+
 }
 else{
 	titleProfile.innerHTML = config_data['username'];
@@ -89,7 +90,6 @@ function createMessage(user, msg, type){
 							</div>';
 		chat.appendChild(msg_node);
 	}
-	console.log(chat.scrollHeight);
 	chat.scrollTop = chat.scrollHeight;
 }
 
@@ -99,7 +99,7 @@ function addChat(msg, type){
 	if(prev && prev.getAttribute("class") == type){
 		prev.appendChild(document.createElement("br"));
 		prev.innerHTML += msg;
-		//prev.appendChild(document.createTextNode(msg));
+
 	}
 	else{
 		var e = document.createElement('div');
@@ -107,6 +107,7 @@ function addChat(msg, type){
 		e.innerHTML = msg;
 		chat.appendChild(e);
 	}
+	chat.scrollTop = chat.scrollHeight;
 }
 
 function addGroup(group){
@@ -157,12 +158,11 @@ function hideElement(e){
 	e.setAttribute("style", "display:none");
 }
 
-var welcomePage = document.getElementById("welcomeContainer");
-var createPage = document.getElementById("createContainer");
-var joinPage = document.getElementById("joinContainer");
-var chatPage = document.getElementById("groupContainer");
-var listening = false;
-var backButton = document.getElementById("backButton");
+var welcomePage = el("welcomeContainer");
+var createPage = el("createContainer");
+var joinPage = el("joinContainer");
+var chatPage = el("groupContainer");
+var backButton = el("backButton");
 
 function showPage(e){
 	hideElement(currPage);
@@ -193,7 +193,7 @@ backButton.addEventListener("click", function(){
 	showPage(welcomePage);
 });
 
-var createButton = document.getElementById("createButton");
+var createButton = el("createButton");
 createButton.addEventListener("click", function(){
 	var topicInput = document.getElementById("createTopic");
 	var createSubmit = document.getElementById("createSubmit");
@@ -208,8 +208,7 @@ createButton.addEventListener("click", function(){
 	showPage(createPage);
 });
 
-var client;
-client = new Client();
+var client = new Client();
 client.on('connect', function(){
 	addChat("Connecting to Server...", "log");
 });
@@ -241,23 +240,30 @@ client.on('user_update', function(){
 	updateUserList(client.users);
 });
 
-document.getElementById("createSubmit").addEventListener("click", function(e){
+el("createSubmit").addEventListener("click", function(e){
+	
+	/*
 	Server.server.listen(0);
 	Server.topic = document.getElementById("createTopic").value;
-	Scanner.broadcast(Server);
 	listening = true;
-	//console.log();
+
 	client = new Client();
+	client.name = config_data['username'];
 	client.connect('localhost', Server.server.address().port);
+	Server.setOwner({name:client.name});
+	Scanner.broadcast(Server);
+	*/
+
+	client = Server.start(el('createTopic').value, config_data['username']);
 	showPage(chatPage);
 	addChat("Server Listening...", "admin");
 });
 
 
-var joinSubmit = document.getElementById("joinSubmit");
-var refreshButton = document.getElementById("joinRefreshButton");
-var joinLoader = document.getElementById("joinLoader");
-var joinEmpty = document.getElementById("joinEmpty");
+var joinSubmit = el("joinSubmit");
+var refreshButton = el("joinRefreshButton");
+var joinLoader = el("joinLoader");
+var joinEmpty = el("joinEmpty");
 var scanTimer;
 
 var selected_group = null;
@@ -312,6 +318,7 @@ joinSubmit.addEventListener("click", function(){
 	if(!grp) return false;
 
 	client = new Client();
+	client.name = config_data['username'];
 	client.connect(grp.address, grp.port);
 	showPage(chatPage);
 });
@@ -339,7 +346,6 @@ chatForm.addEventListener("submit", function(e){
 		if(command[1] == 'connect' || command[1] == 'c'){
 			client = new Client();
 			client.connect('localhost', 7000);
-			//console.log("conn");
 		}
 		else{
 			client.send(command[1], command[2]);
@@ -347,6 +353,7 @@ chatForm.addEventListener("submit", function(e){
 	}
 	else{
 		client.send("m", inputText);
+		createMessage(client, {body:inputText, time:'0'}, 'self');
 	}
 
 });
